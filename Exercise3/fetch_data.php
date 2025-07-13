892cac3d9bcb6526637aca5fa9577cb995d6f6f0
Wong-Mei-Ting <meting116@gmail.com>
2025-07-13 19:02:41 +0800

    Add files via upload

---
+++ Exercise3/fetch_data.php
@@ -0,0 +1,20 @@
+<?php
+include "db_connect.php";
+
+$sql = "SELECT * FROM persons";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // Output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "<p>ID: " . $row["ID"] . " - Name: " .
+             $row["FirstName"] . " " . $row["LastName"] . "<br>";
+
+        echo "Address: " . $row["Adress"] . " <br>City:" . $row["City"] . "</p>";
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
+?>
